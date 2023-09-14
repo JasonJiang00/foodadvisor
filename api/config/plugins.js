27b04a70b043c17fe536e81bcd2031f29d1ea943
jiangfan <jiang.fan@fabernovel.com>
2023-09-14 12:30:02 +0800

    upload oss

--- api/config/plugins.js
+++ api/config/plugins.js
@@ -4,5 +4,18 @@ module.exports = ({ env }) => ({
     config: {
       model: 'scheduler',
     },
+  },
+  upload: {
+    config: {
+      provider: 'strapi-provider-upload-oss', // full package name is required
+      providerOptions: {
+        accessKeyId: env('ACCESS_KEY_ID'),
+        accessKeySecret: env('ACCESS_KEY_SECRET'),
+        region: env('REGION'),
+        bucket: env('BUCKET'),
+        uploadPath: env('UPLOAD_PATH'),
+        baseUrl: env('BASE_URL'),
+      }
+    }
   }
 });
